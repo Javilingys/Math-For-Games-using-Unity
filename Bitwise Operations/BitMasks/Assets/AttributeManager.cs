97bd25169487e78adce35f96be1d0a166d1502eb
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-06 21:10:59 +0700

    bit masks example

---
+++ Bitwise Operations/BitMasks/Assets/AttributeManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AttributeManager : MonoBehaviour
+{
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+
+
+    public Text attributeDisplay;
+    public int attributes = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "MAGIC")
+        {
+            attributes |= MAGIC;
+        }
+        else if (other.gameObject.tag == "INTELLIGENCE")
+        {
+            attributes |= INTELLIGENCE;
+        }
+        else if (other.gameObject.tag == "CHARISMA")
+        {
+            attributes |= CHARISMA;
+        }
+        else if (other.gameObject.tag == "FLY")
+        {
+            attributes |= FLY;
+        }
+        else if (other.gameObject.tag == "INVISIBLE")
+        {
+            attributes |= INVISIBLE;
+        }
+        else if (other.gameObject.tag == "ANTIMAGIC")
+        {
+            attributes &= ~MAGIC;
+        }
+        else if (other.gameObject.tag == "REMOVE")
+        {
+            attributes &= ~ (INTELLIGENCE | MAGIC);
+        }
+        else if (other.gameObject.tag == "ADD")
+        {
+            attributes |= (INTELLIGENCE | MAGIC | CHARISMA);
+        }
+        else if (other.gameObject.tag == "RESET")
+        {
+            attributes = 0;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+    }
+       
+}
---
+++ Bitwise Operations/BitMasks/Assets/DoorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    int doorType = AttributeManager.MAGIC;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.GetComponent<AttributeManager>().attributes & doorType) != 0)
+        {
+            this.GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
