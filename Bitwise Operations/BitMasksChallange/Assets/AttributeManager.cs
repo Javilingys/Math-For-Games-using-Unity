1d30566349f2ce38b9607ab8e193f1e6bc34d6d3
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-06 21:45:28 +0700

    bit mask challenge

---
+++ Bitwise Operations/BitMasksChallange/Assets/AttributeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AttributeManager : MonoBehaviour
+{
+    static public int BLUEKEY = 4;
+    static public int REDKEY = 2;
+    static public int GREENKEY = 1;
+
+    public Text attributeDisplay;
+    public int attributes = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "BLUE_KEY")
+        {
+            attributes |= BLUEKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "RED_KEY")
+        {
+            attributes |= REDKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "GREEN_KEY")
+        {
+            attributes |= GREENKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "GOLD_KEY")
+        {
+            attributes |= (BLUEKEY | REDKEY | GREENKEY);
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+    }
+       
+}
---
+++ Bitwise Operations/BitMasksChallange/Assets/DoorManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    int doorType = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if((collision.gameObject.GetComponent<AttributeManager>().attributes & doorType) == doorType)
+        {
+            this.GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        this.GetComponent<BoxCollider>().isTrigger = false;
+        other.gameObject.GetComponent<AttributeManager>().attributes &= ~doorType;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.gameObject.tag == "BLUE_DOOR") doorType = AttributeManager.BLUEKEY;
+        if(this.gameObject.tag == "RED_DOOR") doorType = AttributeManager.REDKEY;
+        if(this.gameObject.tag == "GREEN_DOOR") doorType = AttributeManager.GREENKEY;
+        if (this.gameObject.tag == "PURPLE_DOOR") 
+            doorType = (AttributeManager.REDKEY | AttributeManager.BLUEKEY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
