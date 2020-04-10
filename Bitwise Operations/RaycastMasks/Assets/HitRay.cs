2bfb21058b2a2e144760b6f1cfc4274e2efc221b
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-10 13:32:13 +0700

    show how unity using int32 to store Layers and use masks

---
+++ Bitwise Operations/RaycastMasks/Assets/HitRay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitRay : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        int layerMask = 1 << 10; // 00010000000000
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
+            out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position,
+                transform.TransformDirection(Vector3.forward) * hit.distance,
+                Color.yellow);
+            Debug.Log("Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position,
+                transform.TransformDirection(Vector3.forward) * 1000,
+                Color.red);
+            Debug.Log("Missed");
+        }
+    }
+}
