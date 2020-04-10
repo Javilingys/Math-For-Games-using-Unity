e8da09fc613f4aa9e27a2e174bb30440a973bd53
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-10 15:07:15 +0700

    Hit only cub and sphere using bit shifting and masks

--- Bitwise Operations/RaycastMasks/Assets/HitRay.cs
+++ Bitwise Operations/RaycastMasks/Assets/HitRay.cs
@@ -7,16 +7,25 @@ public class HitRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int layerMask = 1 << 10; // 00010000000000
+        /* int cubeLayerMask = 1 << 10; // 00010000000000
+        int sphereLayerMask = 1 << 9;
+
+        int layerMask = cubeLayerMask | sphereLayerMask;
+
+        //layerMask = ~layerMask; */
+
+        int layerMask = ((1 << 10) | (1 << 9)); 
+
         RaycastHit hit;
 
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
             out hit, Mathf.Infinity, layerMask))
         {
             Debug.DrawRay(transform.position,
                 transform.TransformDirection(Vector3.forward) * hit.distance,
                 Color.yellow);
             Debug.Log("Hit");
+            Debug.Log(hit.collider);
         }
         else
         {
