f6aa2578cb95c4513a7e26d3977b69f5caa09448
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-10 20:48:37 +0700

    added cells and cells count methods

--- Bitwise Operations/Bitboard/Assets/CreateBoard.cs
+++ Bitwise Operations/Bitboard/Assets/CreateBoard.cs
@@ -9,6 +9,7 @@ public class CreateBoard : MonoBehaviour
     public GameObject[] tilePrefabs;
     public GameObject housePrefab;
     public Text score;
+    long dirtBB = 0; // 64 bits
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,37 @@ public class CreateBoard : MonoBehaviour
                     pos,
                     Quaternion.identity);
                 tile.name = tile.tag + "_" + r + "_" + c;
+                if (tile.tag == "Dirt")
+                {
+                    dirtBB = SetCellState(dirtBB, r, c);
+                    //PrintBB("Dirt", dirtBB);
+                }
             }
+        Debug.Log($"Dirt cells = {CellCount(dirtBB)}");
+    }
+
+    void PrintBB(string name, long BB)
+    {
+        Debug.Log(name + ": " + Convert.ToString(BB, 2).PadLeft(64, '0'));
+    }
+
+    long SetCellState(long bitboard, int row, int col)
+    {
+        long newBit = 1L << (row * 8 + col);
+        return (bitboard |= newBit);
+    }
+
+    int CellCount(long bitboard)
+    {
+        int count = 0;
+        long bb = bitboard;
+        while(bb != 0)
+        {
+            bb &= bb - 1;
+            Debug.Log($"bb = {Convert.ToString(bb, 2).PadLeft(64, '0')}");
+            count++;
+        }
+        return count;
     }
 
     // Update is called once per frame
