87d5b00091ecf9a98b7616cb304913859e332a41
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-12 21:19:14 +0700

    added a fuel instantiate

---
+++ Location/2DGame/Assets/ObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject objPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100),
+                                                            Random.Range(-100, 100),
+                                                            objPrefab.transform.position.z),
+                                                            Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
