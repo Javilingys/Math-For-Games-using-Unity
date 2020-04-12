98e384e4b1fd2daefacbcd8cf10c5f7bc41ac757
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-12 21:08:30 +0700

    added a following camera to player script

---
+++ Location/2DGame/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.position.x,
+                                            player.position.y,
+                                            this.transform.position.z);
+    }
+}
