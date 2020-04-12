09d062627d30c7e243b0d0a229e800db85ad6542
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-12 15:29:27 +0700

    Craeted a simple class "Coord" that show how work Transform underhood

---
+++ Location/Points2D/Assets/Coords.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coords
+{
+    float x;
+    float y;
+    float z;
+
+    public Coords(float _x, float _y)
+    {
+        x = _x;
+        y = _y;
+        z = -1;
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + "," + y + "," + z + ")";
+    }
+}
