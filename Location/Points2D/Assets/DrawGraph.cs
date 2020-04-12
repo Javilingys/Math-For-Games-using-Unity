dc7228b88c13dc9dd91fb5ca97e77fc19210023f
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-12 20:44:35 +0700

    draw grid example

--- Location/Points2D/Assets/DrawGraph.cs
+++ Location/Points2D/Assets/DrawGraph.cs
@@ -5,29 +5,39 @@ using UnityEngine;
 public class DrawGraph : MonoBehaviour
 {
     public int size = 20;
+    public int xMax = 200;
+    public int yMax = 200;
 
+    Coords startPointYAxis;
+    Coords endPointYAxis;
 
-    Coords startPointYAxis = new Coords(0, 100);
-    Coords endPointYAxis = new Coords(0, -100);
-
-    Coords startPointXAxis = new Coords(160, 0);
-    Coords endPointXAxis = new Coords(-160, 0);
+    Coords startPointXAxis;
+    Coords endPointXAxis;
     // Start is called before the first frame update
     void Start()
     {
+        startPointXAxis = new Coords(xMax, 0);
+        endPointXAxis = new Coords(-xMax, 0);
+
+        startPointYAxis = new Coords(0, yMax);
+        endPointYAxis = new Coords(0, -yMax);
+
         // y axis
         Coords.DrawLine(startPointYAxis, endPointYAxis, 1, Color.green);
         // x axis
         Coords.DrawLine(startPointXAxis, endPointXAxis, 1, Color.red);
 
-        for (int x = -160; x <= 160; x += size)
+        // How many gaps
+        int xOffset = (int)(xMax / (float)size);
+        for (int x = -xOffset*size; x <= xOffset * size; x += size)
         {
-            Coords.DrawLine(new Coords(x, -100), new Coords(x, 100), 0.5f, Color.white);
+            Coords.DrawLine(new Coords(x, -yMax), new Coords(x, yMax), 0.5f, Color.white);
         }
 
-        for (int y = -100; y <= 100; y += size)
+        int yOffset = (int)(yMax / (float)size);
+        for (int y = -yOffset*size; y <= yOffset*size; y += size)
         {
-            Coords.DrawLine(new Coords(-160, y), new Coords(160, y), 0.5f, Color.white);
+            Coords.DrawLine(new Coords(-xMax, y), new Coords(xMax, y), 0.5f, Color.white);
         }
     }
 
