b617615eb4e94c53ee1d95d7105048153123e0f9
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-12 17:16:06 +0700

    draw X and Y axis

--- Location/Points2D/Assets/Coords.cs
+++ Location/Points2D/Assets/Coords.cs
@@ -34,4 +34,16 @@ public class Coords
         lineRenderer.startWidth = width;
         lineRenderer.endWidth = width;
     }
+
+    static public void DrawLine(Coords startPoint, Coords endPoint, float width, Color colour)
+    {
+        GameObject line = new GameObject("Line_" + startPoint.ToString() + "_" + endPoint.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.SetPosition(0, new Vector3(startPoint.x, startPoint.y, startPoint.z));
+        lineRenderer.SetPosition(1, new Vector3(endPoint.x, endPoint.y, endPoint.z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
 }
--- Location/Points2D/Assets/DrawLines.cs
+++ Location/Points2D/Assets/DrawLines.cs
@@ -7,11 +7,23 @@ public class DrawLines : MonoBehaviour
 
     Coords point = new Coords(10, 20);
 
+    Coords startPointYAxis = new Coords(0, 100);
+    Coords endPointYAxis = new Coords(0, -100);
+
+    Coords startPointXAxis = new Coords((float)(100 * 1.6), 0);
+    Coords endPointXAxis = new Coords(-(float)(100 * 1.6), 0);
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(point.ToString());
-        Coords.DrawPoint(point, 2, Color.green);
+        //Coords.DrawPoint(new Coords(0, 0), 2, Color.magenta);
+        Coords.DrawPoint(point, 2, Color.yellow);
+
+        // y axis
+        Coords.DrawLine(startPointYAxis, endPointYAxis, 0.5f, Color.green);
+        // x axis
+        Coords.DrawLine(startPointXAxis, endPointXAxis, 1, Color.red);
     }
 
     // Update is called once per frame
