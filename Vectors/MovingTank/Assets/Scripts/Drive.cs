d44114eb33351cdf1899f99654797367efeed699
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-14 09:38:18 +0700

    starter pack

---
+++ Vectors/MovingTank/Assets/Scripts/Drive.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A very simplistic car driving on the x-z plane.
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, translation, 0);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, 0, -rotation);
+    }
+}
