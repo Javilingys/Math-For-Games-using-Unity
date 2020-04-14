e056ae52cfa485e8a7c88c6ce3b57df1c49604f7
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-14 18:45:49 +0700

    moving vectors

--- Vectors/MovingTank/Assets/Scripts/Drive2.cs
+++ Vectors/MovingTank/Assets/Scripts/Drive2.cs
@@ -7,17 +7,21 @@ public class Drive2 : MonoBehaviour
 {
     float speed = 0.01f;
     public GameObject fuel;
+    Vector3 direction;
+    float stoppingDistance = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = fuel.transform.position - this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = fuel.transform.position - this.transform.position;
-        this.transform.position += direction * speed;   
+        if(Vector3.Distance(this.transform.position, fuel.transform.position) > stoppingDistance)
+        {
+            this.transform.position += direction * speed;
+        }
     }
 }
