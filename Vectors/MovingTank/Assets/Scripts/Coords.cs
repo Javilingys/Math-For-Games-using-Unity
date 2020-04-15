8423ec4c7d51d11c86c74b836f8a0c068dec0ad7
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-15 13:26:58 +0700

    Added a empty HolisticMath class for low-level calculate Hormal Form, Distance and Square

---
+++ Vectors/MovingTank/Assets/Scripts/Coords.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coords
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public Coords(float _x, float _y)
+    {
+        X = _x;
+        Y = _y;
+        Z = -1;
+    }
+
+    public Coords(float _x, float _y, float _z)
+    {
+        X = _x;
+        Y = _y;
+        Z = _z;
+    }
+
+    public override string ToString()
+    {
+        return "(" + X + "," + Y + "," + Z + ")";
+    }
+
+    public Vector3 ToVector()
+    {
+        return new Vector3(X, Y, Z);
+    }
+
+    static public void DrawLine(Coords startPoint, Coords endPoint, float width, Color colour)
+    {
+        GameObject line = new GameObject("Line_" + startPoint.ToString() + "_" + endPoint.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(startPoint.X, startPoint.Y, startPoint.Z));
+        lineRenderer.SetPosition(1, new Vector3(endPoint.X, endPoint.Y, endPoint.Z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+
+    static public void DrawLine(Coords position, float width, Color colour)
+    {
+        GameObject line = new GameObject("Line_" + position.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(position.X - width / 3.0f, position.Y - width / 3.0f, position.Z));
+        lineRenderer.SetPosition(1, new Vector3(position.X + width / 3.0f, position.Y + width / 3.0f, position.Z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+}
---
+++ Vectors/MovingTank/Assets/Scripts/HolisticMath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolisticMath : MonoBehaviour
+{
+   static public Coords GetNormal(Coords vector)
+    {
+
+    }
+
+    static public float Distance(Coords point1, Coords point2)
+    {
+
+    }
+
+    static public float Square(float value)
+    {
+
+    }
+}
