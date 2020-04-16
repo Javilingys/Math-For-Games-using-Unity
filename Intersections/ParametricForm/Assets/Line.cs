fe7c97d9044ca05be5b186278f1d9b038b32b60c
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-16 21:41:20 +0700

    teleporting sphere in the center between two cubes

---
+++ Intersections/ParametricForm/Assets/Line.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line
+{
+    Coords A;
+    Coords B;
+    Coords v;
+
+    public Line(Coords _A, Coords _B)
+    {
+        A = _A;
+        B = _B;
+        v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+    }
+
+    public Coords GetPointAt(float t)
+    {
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+}
---
+++ Intersections/ParametricForm/Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    Line line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = new Line(new Coords(start.position),
+                        new Coords(end.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = line.GetPointAt(0.5f).ToVector();
+    }
+}
