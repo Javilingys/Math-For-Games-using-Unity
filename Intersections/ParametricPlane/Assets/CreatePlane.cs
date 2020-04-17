53f41d15279d6f811da71dff9351fbd48378ec7a
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-17 22:06:41 +0700

    create plane

--- Intersections/ParametricPlane/Assets/CreatePlane.cs
+++ Intersections/ParametricPlane/Assets/CreatePlane.cs
@@ -15,9 +15,9 @@ public class CreatePlane : MonoBehaviour
         plane = new Plane(new Coords(A.position),
                             new Coords(B.position),
                             new Coords(C.position));
-        for (float s = 0; s < 1; s += 0.1f)
+        for (float s = -5; s < 5; s += 0.1f)
         {
-            for(float t = 0; t < 1; t += 0.1f)
+            for(float t = -5; t < 5; t += 0.1f)
             {
                 GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 sphere.transform.position = plane.Lerp(s, t).ToVector();
