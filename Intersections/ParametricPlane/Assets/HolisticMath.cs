1929772b637acf8095c051c6fc527fce050eb685
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-17 21:59:32 +0700

    create a plane

---
+++ Intersections/ParametricPlane/Assets/HolisticMath.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolisticMath
+{
+    static public float Square(float value)
+    {
+        return value * value;
+    }
+
+    static public float Distance(Coords point1, Coords point2)
+    {
+        float diffSquared = Square(point1.x - point2.x) + 
+                            Square(point1.y - point2.y) + 
+                            Square(point1.z - point2.z);
+        float squareRoot = Mathf.Sqrt(diffSquared);
+        //if you are interested in how the Sqrt is calculated see
+        //https://www.codeproject.com/Articles/570700/SquareplusRootplusalgorithmplusforplusC
+        return squareRoot;
+
+    }
+
+    static public Coords Lerp(Coords A, Coords B, float t)
+    {
+        t = Mathf.Clamp(t, 0, 1);
+        Coords v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+
+    static public Coords GetNormal(Coords vector)
+    {
+        float length = Distance(new Coords(0, 0, 0), vector);
+        vector.x /= length;
+        vector.y /= length;
+        vector.z /= length;
+
+        return vector;
+    }
+
+    static public float Dot(Coords vector1, Coords vector2)
+    {
+        return (vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z);
+    }
+
+    static public float Angle(Coords vector1, Coords vector2)
+    {
+        float dotDivide = Dot(vector1, vector2) / (Distance(new Coords(0, 0, 0), vector1) * Distance(new Coords(0, 0, 0), vector2));
+        return Mathf.Acos(dotDivide);  //radians.  For degrees * 180/Mathf.PI;
+    }
+
+    static public Coords Cross(Coords vector1, Coords vector2)
+    {
+        float iMult = vector1.y * vector2.z - vector1.z * vector2.y;
+        float jMult = vector1.z * vector2.x - vector1.x * vector2.z;
+        float kMult = vector1.x * vector2.y - vector1.y * vector2.x;
+        Coords crossProd = new Coords(iMult, jMult, kMult);
+        return crossProd;
+    }
+
+    static public Coords Rotate(Coords vector, float angle, bool clockwise)
+    //angle in radians please
+    {
+        if (clockwise)
+            angle = 2*Mathf.PI - angle;
+
+        float xVal = vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle);
+        float yVal = vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle);
+        return new Coords(xVal, yVal, 0); 
+    }
+}
---
+++ Intersections/ParametricPlane/Assets/Line.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line
+{
+    Coords A;
+    Coords B;
+    Coords v;
+
+    public enum LINETYPE { LINE, SEGMENT, RAY};
+    LINETYPE type;
+
+    public Line(Coords _A, Coords _B, LINETYPE _type)
+    {
+        A = _A;
+        B = _B;
+        type = _type;
+        v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+    }
+
+    public Coords Lerp(float t)
+    {
+        if (type == LINETYPE.SEGMENT)
+            t = Mathf.Clamp(t, 0, 1); // t between 0 and 1
+        else if (type == LINETYPE.RAY && t < 0)
+            t = 0;
+
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+}
