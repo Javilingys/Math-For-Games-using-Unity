2ddb62fbb68d915e77c66d12a55110291cdc74bf
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-22 15:41:44 +0700

    line line intersectyion

--- Intersections/LineIntersection/Assets/CreateWall.cs
+++ Intersections/LineIntersection/Assets/CreateWall.cs
@@ -7,6 +7,7 @@ public class CreateWall : MonoBehaviour
     Line wall;
     Line ballPath;
     public GameObject ball;
+    Line trajectory;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,20 @@ public class CreateWall : MonoBehaviour
 
         ballPath = new Line(new Coords(-6, 0, 0), new Coords(100, 0, 0));
         ballPath.Draw(0.1f, Color.yellow);
+
+        ball.transform.position = ballPath.A.ToVector();
+
+        float t = ballPath.IntersectsAt(wall);
+        float s = wall.IntersectsAt(ballPath);
+        if(t == t && s == s)
+        {
+            trajectory = new Line(ballPath.A, ballPath.Lerp(t), Line.LINETYPE.SEGMENT);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ball.transform.position = trajectory.Lerp(Time.time).ToVector();
     }
 }
