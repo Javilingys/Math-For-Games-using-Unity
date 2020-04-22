2a8b197aa1e47198317fed043d49e90febe58a7a
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-22 20:49:10 +0700

    unity intersections

---
+++ Intersections/UnityInterscetions/Assets/PlaneRayIntersection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneRayIntersection : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject quad;
+    //public Transform corner1;
+    //public Transform corner2;
+    //public Transform corner3;
+
+    Plane mPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] vertices = quad.GetComponent<MeshFilter>().mesh.vertices;
+        mPlane = new Plane(quad.transform.TransformPoint(vertices[0]),
+                            quad.transform.TransformPoint(vertices[1]),
+                            quad.transform.TransformPoint(vertices[2]));
+        Debug.Log(vertices[0]);
+        Debug.Log(vertices[1]);
+        Debug.Log(vertices[2]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float t = 0.0f;
+
+            if(mPlane.Raycast(ray, out t))
+            {
+                Vector3 hitPoint = ray.GetPoint(t);
+                sphere.transform.position = hitPoint;
+            }
+        }
+    }
+}
