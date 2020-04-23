f013e220fb89ea87eb631fb7dc44157e7718f24b
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-23 18:03:41 +0700

    create and add matrix

---
+++ Affine Trasformations/Matrices/Assets/Scripts/CreateMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMatrix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] mvals = { 1, 2, 3, 4, 5, 6 };
+        Matrix m = new Matrix(2, 3, mvals);
+
+        float[] nvals = { 1, 2, 3, 4, 5, 6 };
+        Matrix n = new Matrix(2, 3, nvals);
+
+        Matrix answer = m + n;
+
+        Debug.Log(m.ToString() + "\n" + n.ToString() + "\n" + answer.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Affine Trasformations/Matrices/Assets/Scripts/Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Matrix
+{
+    float[] values;
+    int rows;
+    int cols;
+
+    public Matrix(int r, int c, float[] v)
+    {
+        rows = r;
+        cols = c;
+        values = new float[rows * cols];
+        Array.Copy(v, values, rows * cols);
+    }
+
+    public override string ToString()
+    {
+        string matrix = "";
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                matrix += values[r * cols + c] + " ";
+            }
+            matrix += "\n";
+        }
+
+        return matrix;
+    }
+
+    static public Matrix operator + (Matrix a, Matrix b)
+    {
+        if (a.rows != b.rows || a.cols != b.cols)
+        {
+            return null;
+        }
+        Matrix result = new Matrix(a.rows, a.cols, a.values);
+        int length = a.rows * a.cols;
+        for (int i = 0; i < length; i++)
+        {
+            result.values[i] += b.values[i];
+        }
+        return result;
+    }
+}
