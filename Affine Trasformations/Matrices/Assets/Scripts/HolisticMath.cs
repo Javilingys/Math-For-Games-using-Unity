8423eb71b096aa105eea34cb83988dc2c499f1cf
Alexey Belov <30364150+Javilingys@users.noreply.github.com>
2020-04-23 17:13:13 +0700

    matrix starter pack

---
+++ Affine Trasformations/Matrices/Assets/Scripts/HolisticMath.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolisticMath
+{
+    static public Coords GetNormal(Coords vector)
+    {
+        float length = Distance(new Coords(0, 0, 0), vector);
+        vector.x /= length;
+        vector.y /= length;
+        vector.z /= length;
+
+        return vector;
+    }
+
+    static public float Distance(Coords point1, Coords point2)
+    {
+        float diffSquared = Square(point1.x - point2.x) + 
+                            Square(point1.y - point2.y) + 
+                            Square(point1.z - point2.z);
+        float squareRoot = Mathf.Sqrt(diffSquared);
+        return squareRoot;
+
+    }
+
+    static public Coords Lerp(Coords A, Coords B, float t)
+    {
+        t = Mathf.Clamp(t, 0, 1);
+        Coords v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+
+    static public float Square(float value)
+    {
+        return value * value;
+    }
+
+    static public float Dot(Coords vector1, Coords vector2)
+    {
+        return (vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z);
+    }
+
+    static public float Angle(Coords vector1, Coords vector2)
+    {
+        float dotDivide = Dot(vector1, vector2) /
+                    (Distance(new Coords(0, 0, 0), vector1) * Distance(new Coords(0, 0, 0), vector2));
+        return Mathf.Acos(dotDivide); //radians.  For degrees * 180/Mathf.PI;
+    }
+
+    static public Coords LookAt2D(Coords forwardVector, Coords position, Coords focusPoint)
+    {
+        Coords direction = new Coords(focusPoint.x - position.x, focusPoint.y - position.y, position.z);
+        float angle = HolisticMath.Angle(forwardVector, direction);
+        bool clockwise = false;
+        if (HolisticMath.Cross(forwardVector, direction).z < 0)
+            clockwise = true;
+
+        Coords newDir = HolisticMath.Rotate(forwardVector, angle, clockwise);
+        return newDir;
+    }
+
+    static public Coords Rotate(Coords vector, float angle, bool clockwise) //in radians
+    {
+        if(clockwise)
+        {
+            angle = 2 * Mathf.PI - angle;
+        }
+
+        float xVal = vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle);
+        float yVal = vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle);
+        return new Coords(xVal, yVal, 0);
+    }
+   
+    static public Coords Translate(Coords position, Coords facing, Coords vector)
+    {
+        if (HolisticMath.Distance(new Coords(0, 0, 0), vector) <= 0) return position;
+        float angle = HolisticMath.Angle(vector, facing);
+        float worldAngle = HolisticMath.Angle(vector, new Coords(0, 1, 0));
+        bool clockwise = false;
+        if (HolisticMath.Cross(vector, facing).z < 0)
+            clockwise = true;
+
+        vector = HolisticMath.Rotate(vector, angle + worldAngle, clockwise);
+
+        float xVal = position.x + vector.x;
+        float yVal = position.y + vector.y;
+        float zVal = position.z + vector.z;
+        return new Coords(xVal, yVal, zVal);
+    }
+
+    static public Coords Cross(Coords vector1, Coords vector2)
+    {
+        float xMult = vector1.y * vector2.z - vector1.z * vector2.y;
+        float yMult = vector1.z * vector2.x - vector1.x * vector2.z;
+        float zMult = vector1.x * vector2.y - vector1.y * vector2.x;
+        Coords crossProd = new Coords(xMult, yMult, zMult);
+        return crossProd;
+    }
+}
---
+++ Affine Trasformations/Matrices/Assets/Scripts/Line.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line
+{
+    public Coords A;
+    Coords B;
+    public Coords v;
+
+    public enum LINETYPE { LINE, SEGMENT, RAY};
+    LINETYPE type;
+
+    public Line(Coords _A, Coords _B, LINETYPE _type)
+    {
+        A = _A;
+        B = _B;
+        type = _type;
+        v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+    }
+
+    public Line(Coords _A, Coords _V)
+    {
+        A = _A;
+        v = _V;
+        B = _A + v;
+        type = LINETYPE.SEGMENT;
+    }
+
+    public Coords Reflect(Coords normal)
+    {
+
+        Coords norm = normal.GetNormal();
+        Coords vnorm = v.GetNormal();
+
+        float d = HolisticMath.Dot(norm, vnorm);
+
+        if (d == 0) return v;
+
+        float vn2 = d * 2;
+        Coords r = vnorm - norm * vn2;
+        return r;
+    }
+
+    public float IntersectsAt(Plane p)
+    {
+        Coords normal = HolisticMath.Cross(p.u, p.v);
+        if (HolisticMath.Dot(normal, v) == 0)
+            return float.NaN;
+        float t = HolisticMath.Dot(normal, p.A - A) / HolisticMath.Dot(normal, v);
+        return t;
+    }
+    
+    public float IntersectsAt(Line l)
+    {
+        if(HolisticMath.Dot(Coords.Perp(l.v),v) == 0)
+        {
+            return float.NaN;
+        }
+        Coords c = l.A - this.A;
+        float t = HolisticMath.Dot(Coords.Perp(l.v), c) / HolisticMath.Dot(Coords.Perp(l.v), v);
+        if((t < 0 || t > 1) && type == LINETYPE.SEGMENT)
+        {
+            return float.NaN;
+        }
+        return t;
+    }
+
+
+    public void Draw(float width, Color col)
+    {
+        Coords.DrawLine(A, B, width, col);
+    }
+
+    public Coords Lerp(float t)
+    {
+        if (type == LINETYPE.SEGMENT)
+            t = Mathf.Clamp(t, 0, 1);
+        else if (type == LINETYPE.RAY && t < 0)
+            t = 0;
+
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+
+    //3D Line Intersection Algorithm
+    //http://inis.jinr.ru/sl/vol1/CMC/Graphics_Gems_1,ed_A.Glassner.pdf
+
+
+}
